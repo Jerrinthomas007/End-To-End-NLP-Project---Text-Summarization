2c38dc4769489198c1d3727dc5ac9819d2f7ba56
Jerrin Thomas <jerryetm007@gmail.com>
2023-11-08 23:01:28 +0530

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-To-End-NLP-Project---Text-Summarizationpyth"
+REPO_NAME = "End-To-End-NLP-Project---Text-Summarization"
 AUTHOR_USER_NAME = "Jerrinthomas007"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "jerryetm007@gmail.com"
