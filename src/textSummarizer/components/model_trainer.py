646bf20b7bdc3249693adbc17889a802914fcbd4
Jerrin Thomas <jerryetm007@gmail.com>
2023-11-26 20:12:57 +0530

    model trainer

--- src/textSummarizer/components/model_trainer.py
+++ src/textSummarizer/components/model_trainer.py
@@ -32,21 +32,21 @@ class ModelTrainer:
 
 
         trainer_args = TrainingArguments(
-            output_dir=self.config.root_dir, num_train_epochs=1, warmup_steps=500,
+            output_dir=self.config.root_dir, num_train_epochs=1, warmup_steps=10,
             per_device_train_batch_size=1, per_device_eval_batch_size=1,
             weight_decay=0.01, logging_steps=10,
-            evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
+            evaluation_strategy='steps', eval_steps=10, save_steps=1e6,
             gradient_accumulation_steps=16
         ) 
 
         trainer = Trainer(model=model_pegasus, args=trainer_args,
                   tokenizer=tokenizer, data_collator=seq2seq_data_collator,
-                  train_dataset=dataset_samsum_pt["train"], 
+                  train_dataset=dataset_samsum_pt["test"], 
                   eval_dataset=dataset_samsum_pt["validation"])
         
         trainer.train()
 
-        ## Save model
+         ## Save model
         model_pegasus.save_pretrained(os.path.join(self.config.root_dir,"pegasus-samsum-model"))
         ## Save tokenizer
         tokenizer.save_pretrained(os.path.join(self.config.root_dir,"tokenizer"))
