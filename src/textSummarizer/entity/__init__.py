836aced6d17674108f24ad3681c7b96f1b47b45f
Jerrin Thomas <jerryetm007@gmail.com>
2023-11-27 19:24:25 +0530

    model

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -39,3 +39,12 @@ class ModelTrainerConfig:
     save_steps: float
     gradient_accumulation_steps: int
 
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
+
